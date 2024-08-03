4efe563bdee871400e06bd8e4ab6abae636c7856
heldercavalcante91 <heldercavalcante91@gmail.com>
2024-08-02 21:09:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Código JavaScript pode ser adicionado aqui
 });
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+    document.querySelector('nav ul').classList.toggle('show');
+});
